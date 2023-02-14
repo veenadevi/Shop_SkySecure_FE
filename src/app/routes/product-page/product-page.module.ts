672153ff0e4ee0e3d2d4a4cc32b1ec02045ce631
RanjithAltsys <Ranjith@altsystech.com>
2023-02-14 13:54:08 +0530

    product-list-page-list

--- src/app/routes/product-page/product-page.module.ts
+++ src/app/routes/product-page/product-page.module.ts
@@ -5,17 +5,23 @@ import { RouterModule } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { ProductPageRoutingModule,  ROUTED_COMPONENTS} from './product-page-routing.module';
+import { ProductComponent } from './product/product.component';
+import { MatCard, MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 
 
 
 @NgModule({
   declarations: [
     ROUTED_COMPONENTS,
+    ProductComponent
   ],
   imports: [
     CommonModule,
     ProductPageRoutingModule,
-    NgbModule
+    NgbModule,
+    MatCardModule,
+    MatIconModule
   ],
   exports: [
     RouterModule
