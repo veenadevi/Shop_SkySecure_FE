8e31270aa08a2ef8301089e6c748052c17f6ac84
Veenaaltsys <veena@altsystech.com>
2023-03-06 12:55:50 +0530

    Altsys Icon click to home page

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,15 +1,20 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
+  
 })
 
 export class HeaderComponent {
 
-
+  constructor(
+    
+    private router : Router
+  ){}
 
   /**
    * Click Functions
@@ -19,6 +24,10 @@ export class HeaderComponent {
 
   public cartFynction(){
     
+  }
+
+  public goToHomePage(){
+    this.router.navigate(['/']);
   }
 
 }
