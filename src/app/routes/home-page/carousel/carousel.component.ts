cc8de9c8baeb2a998034709a4b2d9599a6f7b0cd
Vignesh Sankaran <vignesh@altsystech.com>
2023-04-20 11:20:44 +0530

    task: Carousel Change

--- src/app/routes/home-page/carousel/carousel.component.ts
+++ src/app/routes/home-page/carousel/carousel.component.ts
@@ -1,6 +1,8 @@
 import { Component , OnInit} from '@angular/core';
 import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 
+//import carousel1 from '../../../../assets/images/carousel1.png';
+
 @Component({
   selector: 'carousel',
   templateUrl: './carousel.component.html',
@@ -13,45 +15,45 @@ export class CarouselComponent {
   event_list = [
     {
       event:' Event 1',
-      eventLocation:'Bangalore',
-      eventDescription:'In bangalore, first event is going to happen. Please be careful about it',
-      img: 'https://picsum.photos/900/500?random&t=1',
+      eventLocation:'',
+      eventDescription:'',
+      img: '../../../../assets/images/banner/MicrosoftDefender.png',
       eventStartDate: new Date('2019/05/20'),
       eventEndingDate: new Date('2019/05/24')
     },
      {
       event:' Event 2',
-      eventLocation:'Dubai',
-      eventDescription:'Dubai is another place to host so,e, first event is going to happen. Please be careful about it',
-      img: 'https://picsum.photos/900/500?random&t=3',
+      eventLocation:'',
+      eventDescription:'',
+      img: '../../../../assets/images/banner/AzureActiveDirectory.png',
       eventStartDate: new Date('2019/07/28'),
       eventEndingDate: new Date('2019/07/30')
     },
      {
       event:' Event 3',
-      eventLocation:'New York',
-      eventDescription:'NewYork sits on top of event hosting',
-      img: 'https://picsum.photos/900/500?random&t=4',
+      eventLocation:'',
+      eventDescription:'',
+      img: '../../../../assets/images/banner/Microsoft.png',
       eventStartDate: new Date('2020/05/20'),
       eventEndingDate: new Date('2020/05/24')
     },
      {
       event:' Event 4',
-      eventLocation:'Singapore',
-      eventDescription:'Singapore is another great hosting city',
-      img: 'https://picsum.photos/900/500?random&t=6',
+      eventLocation:'',
+      eventDescription:'',
+      img: '../../../../assets/images/banner/MicrosoftPurview.png',
       eventStartDate: new Date('2018/05/20'),
       eventEndingDate: new Date('2018/05/24')
     },
     {
       event:' Event 5',
-      eventLocation:'Berlin',
-      eventDescription: 'Berlin is best place to hang on',
-      img: 'https://picsum.photos/900/500?random&t=7',
+      eventLocation:'',
+      eventDescription: '',
+      img: '../../../../assets/images/banner/MicrosoftIntune.png',
       eventStartDate: new Date('2017/07/10'),
       eventEndingDate: new Date('2017/08/14')
     },
-    {
+    /*{
       event:' Event 6',
       eventLocation:'Mumbai',
       eventDescription:'Mumbai is hub of startups',
@@ -66,7 +68,7 @@ export class CarouselComponent {
       img: 'https://picsum.photos/900/500?random&t=6',
       eventStartDate: new Date(),
       eventEndingDate: new Date()
-    },
+    },*/
   ]
 
   upcoming_events =  this.event_list.filter( event => event.eventStartDate > new Date());
