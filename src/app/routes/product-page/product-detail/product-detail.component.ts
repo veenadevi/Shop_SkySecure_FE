8ea84332ec7bfc4ddd9ee1335679e2dc6593bee3
jeevanandham97 <jeevanandham@altsystech.com>
2023-06-26 16:40:21 +0530

    till done

--- src/app/routes/product-page/product-detail/product-detail.component.ts
+++ src/app/routes/product-page/product-detail/product-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { MsalService } from '@azure/msal-angular';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
@@ -16,8 +16,73 @@ import { MetadataStore } from 'src/shared/stores/metadata.store';
 })
 export class ProductDetailComponent implements OnInit{
   products = [];
+  links = ['#description', '#feature', '#specification','#reviews', '#compProd', '#bundles','#simProd'];
+  titles = ['Description', 'Features', 'Specification','Reviews','compare produscts','Bundles','Similar Products'];
+  activeLink = this.links[0];
+  myColor = '';
 
+  @ViewChild('descriptionRef') descriptionRef!: ElementRef;
+  @ViewChild('featureRef') featureRef!: ElementRef;
+  @ViewChild('specificationRef') specificationRef!: ElementRef;
+  @ViewChild('reviewsRef') reviewsRef!: ElementRef;
+  @ViewChild('compProdRef') compProdRef!: ElementRef;
+  @ViewChild('bundlesRef') bundlesRef!: ElementRef;
+  @ViewChild('simProdRef') simProdRef!: ElementRef;
+  @ViewChild('section2Ref') section2Ref!: ElementRef;
 
+  scrollToSection(sectionId: any): void {
+
+    this.activeLink=sectionId;
+    sectionId  = sectionId.slice(1);
+    let section;
+    if(sectionId === 'description') {
+      section = this.descriptionRef.nativeElement
+    } else if (sectionId === 'feature') {
+      section = this.featureRef.nativeElement
+    } else if (sectionId === 'specification') {
+      section = this.specificationRef.nativeElement
+    }
+    else if (sectionId === 'reviews') {
+      section = this.reviewsRef.nativeElement
+    }
+    else if (sectionId === 'compProd') {
+      section = this.compProdRef.nativeElement
+    }
+    else if (sectionId === 'bundles') {
+      section = this.bundlesRef.nativeElement
+    }
+    else if (sectionId === 'simProd') {
+      section = this.simProdRef.nativeElement
+    }
+    
+
+    // const section = sectionId === 'section1' ? this.section1Ref.nativeElement : this.section2Ref.nativeElement;
+    section.scrollIntoView({ behavior: 'smooth' });
+  }
+  // responsiveOptions: any[] | undefined;
+
+  responsiveOptions = [
+    {
+        breakpoint: '1199px',
+        numVisible: 1,
+        numScroll: 1
+    },
+    {
+        breakpoint: '991px',
+        numVisible: 2,
+        numScroll: 1
+    },
+    {
+        breakpoint: '767px',
+        numVisible: 1,
+        numScroll: 1
+    }
+];
+
+ featureList: any[] = [];
+ productVariants: any[]  =[];
+ dispFeatureList: any[] = [];
+ productName:string;
   public bannerUrl : any;
 
   private subscriptions: Subscription[] = [];
@@ -77,25 +142,36 @@ export class ProductDetailComponent implements OnInit{
     this.onProductLoad = true;*/
   }
 
-  private getProductDetails(productId: string): void {
+  private  getProductDetails(productId: string): void {
     this.onProductLoad = false;
     this.subscriptions.push(
-       this.metaDataSvc.fetchSingleProductDetails(productId).subscribe( response => {
-        this.individualProductDetail$.subscribe();
+      this.metaDataSvc.fetchSingleProductDetails(productId).subscribe( (response) => {
+         this.individualProductDetail$.subscribe();
 
         let fList = [];
-        if(response.featureList?.length > 0) {
+        if (response.featureList?.length > 0) {
           //featureList = response.productFeatureList;
-          fList = response.featureList;
+          this.featureList = response.featureList.slice(0,5);
           //this.productSubCategoryId = response.productFeatureList[0].subCategoryId;
         }
+        if(response.products.name!=null) {
+        this.productName = response.products.name;
+        }
 
+        // this.featureList = fList;
+        console.log("inside", this.featureList);
+        
         this.similarProducts = response.productBundles;
-        this.product = { ...response.products , featureList : response.featureList, productFeatureList: response.productFeatureList, productVariants: response.productVariants, featureListByProductVariants : response.featureListByProductVariants } ;
+        this.product = { products:[...response.products], featureList: response.featureList, productFeatureList: response.productFeatureList, productVariants: response.productVariants, featureListByProductVariants: response.featureListByProductVariants };
+        // console.log("inside", this.product);
         this.onProductLoad = true;
         this.bannerUrl = this.product.bannerURL;
-       
-        
+        this.product.productVariants = [123];
+        console.log("prVar:",this.product.productVariants)
+        this.productVariants=response.productVariants;
+        // this.featureCountEvent();
+        this.setDataValues( {...response.products});
+
         /*let featureList = [];
         // if(response.productFeatureList?.length > 0) {
         //   //featureList = response.productFeatureList;
@@ -115,9 +191,16 @@ export class ProductDetailComponent implements OnInit{
         this.product = { ...response.products , featureList : response.featureList, productFeatureList: response.productFeatureList, productVariants: response.productVariants, featureListByProductVariants : response.featureListByProductVariants } ;
         this.onProductLoad = true;*/
       })
+
+
     );
   }
 
+  public setDataValues(resp:any): void {
+    // this.product.name=resp.product.name;
+    // this.productName = resp.product.name;
+console.log("response values: ", resp);
+  }
   private getSimilerProducts(subCategoryId: String) {
     this.subscriptions.push(
       this.metaDataSvc.fetchAllProductsBySubCategoryIds([subCategoryId]).subscribe(response => {
@@ -133,6 +216,8 @@ export class ProductDetailComponent implements OnInit{
     );
   }
 
+  viewAllFeature = false;
+
   constructor(
     private metaDataSvc : MetadataService,
     private route: ActivatedRoute,
@@ -142,31 +227,49 @@ export class ProductDetailComponent implements OnInit{
     private authService : MsalService,
     private modalService : NgbModal
   ){}
+featureCount=5;
 
   public ngOnInit() : void {
     const productId = this.route.snapshot.paramMap.get('id');
+    this.viewAllFeature=false;
+    
     this.getProductDetails(productId);
+    console.log(this.featureList);
+    // if(this.featureList.length>5 && !this.viewAllFeature){
+    //   this.dispFeatureList = [...this.featureList.slice(0,5)];
+    //   console.log("dispfeatureList",this.featureList);
+    // } else {
+    //   this.dispFeatureList = [...this.featureList];
+    // }
+    // console.log("featureList",this.product);
+    // if(this.product.featureList.length>5) {
+    //   for(let i=0;i<5;i++) {
+    //     this.featureList.push(this.product.featureList[i]);
+    //   }
+    // }  else {
+    //     this.featureList = this.product.featureList;
+    // }
+    this.featureList = this.product.featureList;
+    console.log("featureList",this.dispFeatureList.length);
     //this.getProductDetails2(productId);
+  }
+
+  public featureCountEvent(): void {
+    
+    //  if(this.product.featureList.length>5 && !this.viewAllFeature){
+    //   this.featureList = [...this.product.featureList.slice(0,5)];
+    //   console.log("dispfeatureList",this.featureList);
+    //   this.viewAllFeature = true;
+    // } else {
+      this.featureList = this.product.featureList;
+    // }
   }
 
   images: any[] = [1,2,3,4];
 
 
     position: string = 'left';
-    responsiveOptions: any[] = [
-      {
-          breakpoint: '1024px',
-          numVisible: 5
-      },
-      {
-          breakpoint: '768px',
-          numVisible: 3
-      },
-      {
-          breakpoint: '560px',
-          numVisible: 1
-      }
-  ];
+   
 
   public requestQuote (product : ProductsDetails) : void {
 
