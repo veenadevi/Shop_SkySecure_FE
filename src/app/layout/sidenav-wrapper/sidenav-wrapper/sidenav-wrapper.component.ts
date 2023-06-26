ad300327ba22001abf0d73dd226818ecea58077a
Vignesh Sankaran <vignesh@altsystech.com>
2023-06-26 20:58:57 +0530

    task: Some Basic Designs

--- src/app/layout/sidenav-wrapper/sidenav-wrapper/sidenav-wrapper.component.ts
+++ src/app/layout/sidenav-wrapper/sidenav-wrapper/sidenav-wrapper.component.ts
@@ -16,6 +16,11 @@ export class SidenavWrapperComponent {
     console.log("&&&&&&&& ++++++ ", this.userLoggedInFlag);
   }
 
+
+  public userSubMenu : boolean = false;
+  public dashboardSubMenu : boolean = false;
+  public securitySubMenu : boolean = false;
+
   constructor(
     private loginService : LoginService
   ){}
@@ -23,6 +28,41 @@ export class SidenavWrapperComponent {
 
   public logout() {
     this.loginService.logout();
+  }
+
+
+  public menuExpansion(val){
+
+    this.isExpanded = (val) ? false : true;
+    if(this.isExpanded === false){
+      this.userSubMenu = false;
+      this.dashboardSubMenu = false;
+      this.securitySubMenu = false;
+    }
+  }
+
+
+  public subMenuClick(val){
+
+
+    
+    console.log("+++++++ ", val);
+
+    switch (val) {
+      case 'user':
+          this.userSubMenu = (this.userSubMenu) ? false : true;
+          return;
+      case 'dashboard':
+          this.dashboardSubMenu = (this.dashboardSubMenu) ? false : true;
+          return;
+      case 'security':
+        this.securitySubMenu = (this.securitySubMenu) ? false : true;
+        return;
+
+      default:
+        return null;
+    }
+
   }
 
 }
