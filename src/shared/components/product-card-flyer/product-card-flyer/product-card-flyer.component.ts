840aad9bfe2e2e7f8b8c054e99843cb850a552a2
Vignesh Sankaran <vignesh@altsystech.com>
2023-07-02 09:57:02 +0530

    task: Modal Changes

--- src/shared/components/product-card-flyer/product-card-flyer/product-card-flyer.component.ts
+++ src/shared/components/product-card-flyer/product-card-flyer/product-card-flyer.component.ts
@@ -64,7 +64,7 @@ export class ProductCardFlyerComponent implements OnInit{
         return;
 
       case 'productVariants':
-        this.router.navigate(['/products', product._id]);
+        this.router.navigate(['/products/product-variant-detail', product._id]);
         return;
         
       case 'productBundles':
