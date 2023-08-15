035e5b3b937dd1827fd91ca063d44d7f2809b5b1
VeenaSkysecure <veena@skysecuretech.com>
2023-08-15 22:31:38 +0530

    added google Analytics code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 import {
   trigger,
@@ -9,6 +10,8 @@ import {
 } from '@angular/animations';
 
 import { IdTokenClaims } from '@azure/msal-common';
+import { ActivatedRoute, NavigationEnd, Router, RouterState } from '@angular/router';
+import { DOCUMENT } from '@angular/common';
 
 
 type IdTokenClaimsWithPolicyId = IdTokenClaims & {
@@ -25,9 +28,40 @@ type IdTokenClaimsWithPolicyId = IdTokenClaims & {
 export class AppComponent {
   title = 'altsys_frontend';
     constructor(
-    ) { }
+ 
+      private router: Router,
+    private titleService: Title,
+    @Inject(DOCUMENT) private document: Document
+    ) {
+    this.handleRouteEvents();
+     }
 
-  
+     handleRouteEvents() {
+      this.router.events.subscribe(event => {
+        if (event instanceof NavigationEnd) {
+          const title = this.getTitle(this.router.routerState, this.router.routerState.root).join('-');
+          this.titleService.setTitle(title);
+          gtag('event', 'page_view', {
+            page_title: title,
+            page_path: event.urlAfterRedirects,
+            page_location: this.document.location.href
+          })
+        }
+      });
+    }
+
+    getTitle(state: RouterState, parent: ActivatedRoute): string[] {
+      const data = [];
+      if (parent && parent.snapshot.data && parent.snapshot.data['title']) {
+        data.push(parent.snapshot.data['title']);
+      }
+      if (state && parent && parent.firstChild) {
+        data.push(...this.getTitle(state, parent.firstChild));
+      }
+      return data;
+    }
 }
+
+
 
 
