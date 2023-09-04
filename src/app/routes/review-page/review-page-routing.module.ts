e2c00a838e2156a6c148413bed16c5da0dc1247e
vignesh-Altsys <130962208+vignesh-Altsys@users.noreply.github.com>
2023-09-04 17:04:49 +0530

    review last page design

--- src/app/routes/review-page/review-page-routing.module.ts
+++ src/app/routes/review-page/review-page-routing.module.ts
@@ -4,6 +4,7 @@ import { ReviewPageComponent } from './review-page.component';
 import { ReviewHomeComponent } from './partials/review-home/review-home.component';
 import { ReviewDetailPageComponent } from './partials/review-detail-page/review-detail-page.component';
 import { ReviewRatingPageComponent } from './partials/review-rating-page/review-rating-page.component';
+import { ReviewFinalPageComponent } from './partials/review-final-page/review-final-page.component';
 
 
 const routes: Routes = [{
@@ -27,6 +28,11 @@ const routes: Routes = [{
       canActivate: [],
       path: 'review-rating-page',
       component : ReviewRatingPageComponent
+    },
+    {
+      canActivate: [],
+      path: 'review-final-page',
+      component : ReviewFinalPageComponent
     },
   ]
   
--- src/app/routes/review-page/review-page.module.ts
+++ src/app/routes/review-page/review-page.module.ts
@@ -6,6 +6,8 @@ import { ReviewPageComponent } from './review-page.component';
 import { ReviewHomeComponent } from './partials/review-home/review-home.component';
 import { ReviewDetailPageComponent } from './partials/review-detail-page/review-detail-page.component';
 import { ReviewRatingPageComponent } from './partials/review-rating-page/review-rating-page.component';
+import { ReviewFinalPageComponent } from './partials/review-final-page/review-final-page.component';
+import { MaterialModule } from 'src/app/material.module';
 
 
 
@@ -15,11 +17,19 @@ import { ReviewRatingPageComponent } from './partials/review-rating-page/review-
     ReviewHomeComponent,
     ReviewDetailPageComponent,
     ReviewRatingPageComponent,
+    ReviewFinalPageComponent,
+
    
   ],
   imports: [
     CommonModule,
-    ReviewPageRoutingModule
+    ReviewPageRoutingModule,
+    MaterialModule
+    
+  ],
+  exports: [
+    MaterialModule
   ]
 })
-export class ReviewPageModule { }
+export class ReviewPageModule {
+ }
