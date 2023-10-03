fe75cdaef853df3cad6237f1dd4f088f833f7a83
VeenaSkysecure <veena@skysecuretech.com>
2023-10-03 14:44:14 +0530

    user quote summary fixed

---
+++ src/app/routes/user-profile/partials/quotation-history/partials/product-list-table/product-list-table.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ProductListTableComponent } from './product-list-table.component';
+
+describe('ProductListTableComponent', () => {
+  let component: ProductListTableComponent;
+  let fixture: ComponentFixture<ProductListTableComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ProductListTableComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ProductListTableComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
