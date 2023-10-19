11d31fc044345b806465c815f961086fd1d39544
Swetha Jinka <swetha.jinka@skysecuretech.com>
2023-10-19 14:42:56 +0530

    upload file button
    upload file button

--- src/shared/components/modals/add-compare-product-modal/add-compare-product-modal.component.ts
+++ src/shared/components/modals/add-compare-product-modal/add-compare-product-modal.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { ToasterNotificationService } from 'src/shared/services/toaster-notification.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'add-compare-product-modal',
@@ -8,16 +9,21 @@ import { ToasterNotificationService } from 'src/shared/services/toaster-notifica
   styleUrls: ['./add-compare-product-modal.component.css']
 })
 export class AddCompareProductModalComponent implements OnInit {
+  public productLogo: string;
 
+ 
+  showCompareProductModal: boolean = false;
+  showUploadPoModal: boolean = false;
+  dataForModal: any;
+  modalType: string;
 
-  @Input('request')
-  public request : any;
-
+  @Input('request')  public request : any;
   @Output() passEntry: EventEmitter<any> = new EventEmitter();
 
   constructor(
     public activeModal: NgbActiveModal,
-    private toaster : ToasterNotificationService
+    private toaster : ToasterNotificationService,
+    private http: HttpClient,
   ){}
 
   public headerText = "Unlock Better Choices: Compare Products Today!";
@@ -26,8 +32,35 @@ export class AddCompareProductModalComponent implements OnInit {
     console.log("+_+_+_+_+_+_+_+_+_+ _Val", this.request);
     if(this.request && this.request.screen === 'edit-product-in-accounts'){
       this.headerText = "Please select the Product!";
-    }
+    } 
+    // if (this.modalType === 'compare') {
+    //   this.headerText = 'Unlock Better Choices: Compare Products Today!';
+    //   this.showCompareProductModal = true;
+    //   this.showUploadPoModal = false;
+    //   console.log("IN If modal type")
+    // } else if (this.modalType === 'upload') {
+    //   this.headerText = 'Upload PO';
+    //   this.showCompareProductModal = false;
+    //   this.showUploadPoModal = true;
+    //   console.log("IN else  modal type")
+    // }
   }
+
+  // openModal(modalType: 'compare' | 'upload') {
+  //   console.log(`Opening modal: ${modalType}`);
+  //   if (modalType === 'compare') {
+  //     this.headerText = 'Unlock Better Choices: Compare Products Today!';
+  //     this.showCompareProductModal = true;
+  //     this.showUploadPoModal = false;
+  //   } else if (modalType === 'upload') {
+  //     this.headerText = 'Upload PO';
+  //     this.showCompareProductModal = false;
+  //     this.showUploadPoModal = true;
+  //   }
+  // }
+
+
+
 
   public selectedItem(event){
     
@@ -71,6 +104,26 @@ export class AddCompareProductModalComponent implements OnInit {
 
   public closeModal(){
     this.activeModal.close();
+  }
+
+
+  uploadFile(event: any) {
+    
+    const formData: FormData = new FormData();
+    formData.append('file', event.target.files[0], event.target.files[0].name);
+
+    this.http.post('https://dev-altsys-realize-api.azurewebsites.net/api/file/upload', formData)
+      .subscribe(
+        (response: any) => {
+          
+          this.productLogo = response.filePath;
+          // Handle the response from the server
+        },
+        error => {
+          console.error('Upload error:', error);
+          // Handle the error response
+        }
+      );
   }
 
 }
