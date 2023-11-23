742f096759b8307a7976fd80999c68f4c6cbdb34
VeenaSkysecure <veena@skysecuretech.com>
2023-11-23 11:13:31 +0530

    updated on edit quote

--- src/app/routes/user-profile/partials/quotation-history/partials/product-list-table/product-list-table.component.ts
+++ src/app/routes/user-profile/partials/quotation-history/partials/product-list-table/product-list-table.component.ts
@@ -407,7 +407,7 @@ export class ProductListTableComponent {
 
 
     let request = this.setRequestData();
-    //console.log("+_+_+_+_+_ Res Data ", request);
+ // console.log("+_+_+_+_+_ edit qupte Data ", request);
 
     this.subscription.push(
       this.cartService.editQuotation(request).subscribe(res => {
